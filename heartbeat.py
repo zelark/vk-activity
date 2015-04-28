12b0e941f0c79235040e060de8cc6cd0eed0062f
Alexandr Lark <kvark.lark@gmail.com>
2015-04-28 21:56:24 +0300

    init

---
+++ heartbeat.py
@@ -0,0 +1,56 @@
+from requests import Session
+import json
+import time
+import psycopg2
+import sys
+
+
+def unix2human(unixtime):
+    return time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(unixtime))
+
+def json_parse(response_text):
+    decoder = json.JSONDecoder(strict=False)
+    idx = 0
+    obj, idx = decoder.raw_decode(response_text, idx)
+    return obj['response']
+
+def users_get(user_ids):
+    session = Session()
+    session.headers['Accept'] = 'application/json'
+    session.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    response = session.post(
+        url='http://api.vk.com/method/users.get',
+        params={
+            'user_ids': user_ids,
+            'fields': 'online,last_seen',
+            'name_case': 'Nom',
+            'v': '5.29',
+        }
+    )
+    return json_parse(response.text)
+
+if __name__ == '__main__':
+    
+    teamuse = users_get('teamuse')[0]
+    user_id = teamuse['id']
+    online = teamuse['online']
+    last_seen = teamuse['last_seen']['time']
+
+    db_connection = None
+    try:
+        db_connection = psycopg2.connect(database='d26tqvcs429mhj', user='uaclvvcazghplm', password='mgreNCm6i4bBI-F7q6ywQfGT3M')
+        cursor = db_connection.cursor()
+        cursor.execute(
+            "insert into heartbeat " \
+            "(user_id, online, last_seen, log_time) " \
+            "values (%s, %s, to_timestamp(%s), 'now');",
+            (user_id, online, last_seen))
+        db_connection.commit()
+    
+    except psycopg2.DatabaseError as e:
+        print('Error %s' % e)
+        sys.exit(1)
+
+    finally:
+        if db_connection:
+            db_connection.close()
