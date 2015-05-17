7a5dce32759001a822b896bd53ab4dc5da9b8216
Alexandr Lark <kvark.lark@gmail.com>
2015-05-17 20:26:14 +0300

    delete call of the built-in server

--- rest-server.py
+++ rest-server.py
@@ -45,7 +45,3 @@ class UserAPI(Resource):
         return response, 200
 
 api.add_resource(UserAPI, '/vk/activity/v1.0/users/<int:id>', endpoint='user')
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
