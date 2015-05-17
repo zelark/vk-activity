035e729e0d37d36d75a595f337e4e6d8c4dedd36
Alexandr Lark <kvark.lark@gmail.com>
2015-05-17 19:08:54 +0300

    add getting a port

--- rest-server.py
+++ rest-server.py
@@ -47,4 +47,5 @@ class UserAPI(Resource):
 api.add_resource(UserAPI, '/vk/activity/v1.0/users/<int:id>', endpoint='user')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
